619aa3197b8633fb7e5fab24e4156be4ef0f4ece
Ben Eggett <beneggett@gmail.com>
2012-08-28 01:45:50 -0600

    Updated for Spree 1.2. Namespaced classes, updated to work with Spree.user_class and new checkout state_machines in order.rb

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,91 +1,89 @@
-module Spree
-  Order.class_eval do
-    attr_accessible :store_credit_amount, :remove_store_credits
-    attr_accessor :store_credit_amount, :remove_store_credits
+Spree::Order.class_eval do
+  attr_accessible :store_credit_amount, :remove_store_credits
+  attr_accessor :store_credit_amount, :remove_store_credits
 
-    # the check for user? below is to ensure we don't break the
-    # admin app when creating a new order from the admin console
-    # In that case, we create an order before assigning a user
-    before_save :process_store_credit, :if => "self.user.present? && @store_credit_amount"
-    after_save :ensure_sufficient_credit, :if => "self.user.present? && !self.completed?"
+  # the check for user? below is to ensure we don't break the
+  # admin app when creating a new order from the admin console
+  # In that case, we create an order before assigning a user
+  before_save :process_store_credit, :if => "self.user.present? && @store_credit_amount"
+  after_save :ensure_sufficient_credit, :if => "self.user.present? && !self.completed?"
 
-    validates_with StoreCreditMinimumValidator
+  validates_with StoreCreditMinimumValidator
 
-    def store_credit_amount
-      adjustments.store_credits.sum(:amount).abs
+  def store_credit_amount
+    adjustments.store_credits.sum(:amount).abs
+  end
+
+
+  # override core process payments to force payment present
+  # in case store credits were destroyed by ensure_sufficient_credit
+  def process_payments!
+    if total > 0 && payment.nil?
+      false
+    else
+      ret = payments.each(&:process!)
     end
+  end
 
 
-    # override core process payments to force payment present
-    # in case store credits were destroyed by ensure_sufficient_credit
-    def process_payments!
-      if total > 0 && payment.nil?
-        false
+  private
+
+  # credit or update store credit adjustment to correct value if amount specified
+  #
+  def process_store_credit
+    @store_credit_amount = BigDecimal.new(@store_credit_amount.to_s).round(2)
+
+    # store credit can't be greater than order total (not including existing credit), or the user's available credit
+    @store_credit_amount = [@store_credit_amount, user.store_credits_total, (total + store_credit_amount.abs)].min
+
+    if @store_credit_amount <= 0
+      adjustments.store_credits.destroy_all
+    else
+      if sca = adjustments.store_credits.first
+        sca.update_attributes({:amount => -(@store_credit_amount)})
       else
-        ret = payments.each(&:process!)
+        # create adjustment off association to prevent reload
+        sca = adjustments.store_credits.create(:label => I18n.t(:store_credit) , :amount => -(@store_credit_amount))
       end
     end
 
+    # recalc totals and ensure payment is set to new amount
+    update_totals
+    payment.amount = total if payment
+  end
 
-    private
+  # consume users store credit once the order has completed.
+  Spree::Order.state_machine.after_transition :to => :complete,  :do => :consume_users_credit
 
-    # credit or update store credit adjustment to correct value if amount specified
-    #
-    def process_store_credit
-      @store_credit_amount = BigDecimal.new(@store_credit_amount.to_s).round(2)
 
-      # store credit can't be greater than order total (not including existing credit), or the user's available credit
-      @store_credit_amount = [@store_credit_amount, user.store_credits_total, (total + store_credit_amount.abs)].min
+  def consume_users_credit
+    return unless completed?
+    credit_used = self.store_credit_amount
 
-      if @store_credit_amount <= 0
-        adjustments.store_credits.destroy_all
-      else
-        if sca = adjustments.store_credits.first
-          sca.update_attributes({:amount => -(@store_credit_amount)})
+    user.store_credits.each do |store_credit|
+      break if credit_used == 0
+      if store_credit.remaining_amount > 0
+        if store_credit.remaining_amount > credit_used
+          store_credit.remaining_amount -= credit_used
+          store_credit.save
+          credit_used = 0
         else
-          # create adjustment off association to prevent reload
-          sca = adjustments.store_credits.create(:label => I18n.t(:store_credit) , :amount => -(@store_credit_amount))
+          credit_used -= store_credit.remaining_amount
+          store_credit.update_attribute(:remaining_amount, 0)
         end
       end
-
-      # recalc totals and ensure payment is set to new amount
-      update_totals
-      payment.amount = total if payment
     end
 
-    # consume users store credit once the order has completed.
-    fsm = self.state_machines[:state]
-    fsm.after_transition :to => 'complete', :do => :consume_users_credit
+  end
 
-    def consume_users_credit
-      return unless completed?
-      credit_used = self.store_credit_amount
+  # ensure that user has sufficient credits to cover adjustments
+  #
+  def ensure_sufficient_credit
+    if user.store_credits_total < store_credit_amount
+      # user's credit does not cover all adjustments.
+      adjustments.store_credits.destroy_all
 
-      user.store_credits.each do |store_credit|
-        break if credit_used == 0
-        if store_credit.remaining_amount > 0
-          if store_credit.remaining_amount > credit_used
-            store_credit.remaining_amount -= credit_used
-            store_credit.save
-            credit_used = 0
-          else
-            credit_used -= store_credit.remaining_amount
-            store_credit.update_attribute(:remaining_amount, 0)
-          end
-        end
-      end
-
-    end
-
-    # ensure that user has sufficient credits to cover adjustments
-    #
-    def ensure_sufficient_credit
-      if user.store_credits_total < store_credit_amount
-        # user's credit does not cover all adjustments.
-        adjustments.store_credits.destroy_all
-
-        update!
-      end
+      update!
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Spree::Core::Engine.routes.append do
   namespace :admin do
     resources :store_credits
-    resources :users do
-      resources :store_credits
-    end
   end
 end
