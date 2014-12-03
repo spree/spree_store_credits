9508c88eb8a6bbf64ca99d6492e78f811f53bdeb
Jason Fleetwood-Boldt <jason@mackweldon.com>
2014-12-03 13:11:35 -0500

    backports change made to how files are loaded so that files aren't loaded twice in non-production environments that cache their classes (staging, qa)

--- lib/spree_store_credits.rb
+++ lib/spree_store_credits.rb
@@ -9,9 +9,10 @@ module SpreeStoreCredits
       g.test_framework :rspec
     end
 
+
     def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.env == "production" ? require(c) : load(c)
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
 
